79ee4fe1375ba0dde53eb7176fc684747a81a7a7
DRysaev <denisrysaev1@gmail.com>
2017-12-06 23:34:37 +0300

    Add new project

---
+++ 2_HelloMVCApplication/HelloMvcApplication/HelloMvcApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloMvcApplication.Startup))]
+namespace HelloMvcApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
